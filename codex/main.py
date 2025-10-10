0e605ce8ccea08e5b29c8a14aac2ebe6e4850c7d
Sami Bahij <samibahij@Samis-MacBook-Pro.local>
2025-10-10 19:02:14 +0200

    Ajout du scan des pdf

--- codex/main.py
+++ codex/main.py
@@ -6,6 +6,7 @@ from utils.create_boolean_queries_file import create_boolean_queries_file
 import arxiv
 from arxiv_api import *
 from utils.excel_service import *
+from utils.find_conjectures import *
 
 def parse_args() -> argparse.Namespace:
     p = argparse.ArgumentParser(description="Génère un PICO via Codex puis continue (Unix/macOS).")
@@ -28,6 +29,7 @@ if __name__ == "__main__":
     create_pico_file(pico_prompt)
 
     pico_filename = f"PICO_{research_question}.txt"
+
     boolean_query_prompt = get_prompt_to_generate_boolean_query(
         pico_filename=pico_filename,
         research_question=research_question,
@@ -37,15 +39,17 @@ if __name__ == "__main__":
 
     create_boolean_queries_file(boolean_query_prompt, filename=boolean_query_file)
 
-    query = extract_arxiv_query_py(boolean_query_file)
-
     # @todo Prendre en charges d'autres bases de données (implique de modifier le prompt aussi)
-    # @todo Pour le moment ne pas faire la détection des conjectures, mais faire le fichier Excel.
 
     excel_file = "articles.xlsx"
-    sheet_name = "Feuille 1"
-    create_excel_file(excel_file, sheet_name)
+    sheet_name1 = "Articles"
+    sheet_name2 = "Conjectures"
+    create_excel_file(excel_file, sheet_name1, sheet_name2)
 
-    download_arxiv_pdfs(query, excel_file=excel_file, sheet_name=sheet_name)
+    query = extract_arxiv_query_py(boolean_query_file)
+    download_arxiv_pdfs(query, excel_file=excel_file, sheet_name=sheet_name1)
 
+    find_conjecture_prompt = get_prompt_to_find_conjectures_in_pdfs("", "articles.xlsx")
+    sentinel = run_conjecture_analysis(find_conjecture_prompt)
 
+    print("Process completed.")
