ccca5edc1bc6a8446556f66251494b2ea5fc4e5e
Sami Bahij <samibahij@Samis-MacBook-Pro.local>
2025-10-16 14:21:23 +0200

    refactorisation du service mistral

--- test/mistral/mitralai.py
+++ test/mistral/mitralai.py
@@ -1,5 +1,5 @@
 import os
-from mistralai import Mistral
+from mistralai import Mistral, LibraryOut, DocumentOut
 from dotenv import load_dotenv
 from mistralai.models import File
 
@@ -24,34 +24,51 @@ def create_library(client: Mistral, library_name: str, library_description: str)
     new_library = client.beta.libraries.create(name=library_name, description=library_description)
     return new_library
 
-def get_library(client: Mistral):
+def get_libraries(client: Mistral):
     libraries = client.beta.libraries.list().data
 
     for library in libraries:
         print(library.name, f"with {library.nb_documents} documents.")
+    return libraries
 
-def get_document(libraries, client: Mistral):
-    if len(libraries) == 0:
-        print("No libraries found.")
-    else:
-        doc_list = client.beta.libraries.documents.list(library_id=libraries[0].id).data
-        for doc in doc_list:
-            print(f"{doc.name}: {doc.extension} with {doc.number_of_pages} pages.")
-            print(f"{doc.summary}")
+def get_library(library: LibraryOut, client: Mistral):
+    library = client.beta.libraries.get(library_id=library.id)
+    print(library.name, f"with {library.nb_documents} documents.")
+    return library
+
+def get_document(library: LibraryOut, client: Mistral):
+    doc_list = client.beta.libraries.documents.list(library_id=library.id).data
+    for doc in doc_list:
+        print(f"{doc.name}: {doc.extension} with {doc.number_of_pages} pages.")
+        print(f"{doc.summary}")
 
 
-def delete_library(new_library, client: Mistral):
-    deleted_library = client.beta.libraries.delete(library_id=new_library.id)
+def delete_library(new_library: LibraryOut, client: Mistral):
+    client.beta.libraries.delete(library_id=new_library.id)
 
-def delete_document(new_library, uploaded_doc, client: Mistral):
-    deleted_document = client.beta.libraries.documents.delete(library_id=new_library.id, document_id=uploaded_doc.id)
+def delete_document(new_library: LibraryOut, document: DocumentOut, client: Mistral):
+    client.beta.libraries.documents.delete(library_id=new_library.id, document_id=document.id)
 
-def upload_file(file_path: str, client: Mistral, new_library):
+def upload_file(file_path: str, client: Mistral, new_library: LibraryOut):
     with open(file_path, "rb") as file_content:
         uploaded_doc = client.beta.libraries.documents.upload(
             library_id=new_library.id,
             file=File(fileName="mistral7b.pdf", content=file_content),
         )
+
+def create_library_agent(new_library):
+    library_agent = client.beta.agents.create(
+        model="mistral-medium-2505",
+        name="Document Library Agent",
+        description="Agent used to access documents from the document library.",
+        instructions="Use the  library tool to access external documents.",
+        tools=[{"type": "document_library", "library_ids": [new_library.id]}],
+        completion_args={
+            "temperature": 0.3,
+            "top_p": 0.95,
+        }
+    )
+    return library_agent
 
 if __name__ == "__main__":
     load_dotenv()
@@ -61,4 +78,6 @@ if __name__ == "__main__":
 
     client = Mistral(api_key=api_key)
 
-    get_library(client)
+    library = create_library(client, "test_library", "Ma description")
+
+    get_libraries(client)
