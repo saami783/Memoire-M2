4645b3f3fd214e869b22dd01d9dbfe6b31ad0b78
Sami Bahij <samibahij@samis-macbook-pro.home>
2025-10-18 23:44:34 +0200

    ajout de la detection des conjectures avec mistral

---
+++ mistral/find_conjectures_mistral.py
@@ -0,0 +1,72 @@
+import json
+import os
+from time import sleep
+from dotenv import load_dotenv
+from mistralai import Mistral, LibraryOut, DocumentOut, SDKError
+from mistralai.models import File
+from pathlib import Path
+
+def get_models(client: Mistral):
+    models = client.models.list().data
+    for model in models:
+        print(model.name)
+
+def create_library(client: Mistral, library_name: str, library_description: str):
+    new_library = client.beta.libraries.create(name=library_name, description=library_description)
+    return new_library
+
+def get_libraries(client: Mistral):
+    libraries = client.beta.libraries.list().data
+    return libraries
+
+def get_library(library_id, client: Mistral):
+    library = client.beta.libraries.get(library_id=library_id)
+    return library
+
+def get_documents(library: LibraryOut, client: Mistral):
+    doc_list = client.beta.libraries.documents.list(library_id=library.id).data
+    return doc_list
+
+def get_document(library: LibraryOut, document_id: str, client: Mistral):
+    doc = client.beta.libraries.documents.get(library_id=library.id, document_id=document_id)
+    return doc
+
+def delete_library(library: LibraryOut, client: Mistral):
+    client.beta.libraries.delete(library_id=library.id)
+
+def delete_document(library: LibraryOut, document: DocumentOut, client: Mistral):
+    client.beta.libraries.documents.delete(library_id=library.id, document_id=document.id)
+
+def upload_document(file_path: str, client: Mistral, library: LibraryOut):
+    with open(file_path, "rb") as file_content:
+        uploaded_doc = client.beta.libraries.documents.upload(
+            library_id=library.id,
+            file=File(file_name=file_path, content=file_content),
+        )
+    return uploaded_doc
+
+def create_library_agent(new_library, client):
+    library_agent = client.beta.agents.create(
+        model="mistral-medium-2505",
+        name="Document Library Agent",
+        description="Agent used to access documents from the document library.",
+        instructions="Use the  library tool to access external documents.",
+        tools=[{"type": "document_library", "library_ids": [new_library.id]}],
+        completion_args={
+            "temperature": 0.3,
+            "top_p": 0.95,
+        }
+    )
+    return library_agent
+
+def get_dossier_pdfs(dossier: str):
+    dossier_path = Path(dossier)
+
+    pdf_paths = sorted(
+        p for p in dossier_path.iterdir()
+        if p.is_file() and p.suffix.lower() == ".pdf"
+    )
+
+    noms_pdfs = [p.name for p in pdf_paths]
+
+    return noms_pdfs
