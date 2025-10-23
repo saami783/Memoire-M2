9ed137a8c4559ade8f00f93fef0923fe43fe0457
Sami <sami.bahij1@gmail.com>
2025-10-23 23:08:24 +0200

    Ajout d'un pdf de test avec conjecture et formule mathematique

--- main.py
+++ main.py
@@ -48,57 +48,60 @@ def find_conjectures(dossier_articles: str):
 
     try:
         for pdf in dossier:
-            upload_document(f"{dossier_articles}/{pdf}", pdf, client, library)
-            print(pdf)
+            # document = upload_document(f"{dossier_articles}/{pdf}", pdf, client, library)
+            # print(document)
             sleep(4)
     except SDKError:
         print("Limite d'upload d'articles atteinte.")
 
-    for document in get_documents(library, client):
+    document = get_document(library, "25079da6-7fc3-4262-963b-e500b754e930", client)
+    # for document in get_documents(library, client):
 
-        print(f"###### Détection de conjectures dans le document {document.name} ######")
+    print(f"###### Détection de conjectures dans le document {document.name} ######")
 
-        text_content = client.beta.libraries.documents.text_content(
-            library_id=library.id,
-            document_id=document.id
-        )
+    text_content = client.beta.libraries.documents.text_content(
+        library_id=library.id,
+        document_id=document.id
+    )
 
-        response = get_mistral_reponse(client, model, text_content)
-        export_conjectures_to_json(response, document)
+    response = get_mistral_reponse(client, model, text_content)
+    print(response)
+    export_conjectures_to_json(response, document)
 
 
 if __name__ == "__main__":
-    research_question = get_research_question_arg()
-    boolean_query_file = "boolean_queries.txt"
-    pico_prompt = get_prompt_to_generate_pico(research_question, f"PICO{research_question}.txt")
-
-    create_pico_file(pico_prompt)
-
-    pico_filename = f"PICO_{research_question}.txt"
-
-    boolean_query_prompt = get_prompt_to_generate_boolean_query(
-        pico_filename=pico_filename,
-        research_question=research_question,
-        output_file_name_boolean_queries="boolean_queries",
-        doc_boolean_queries="doc_boolean_queries",
-    )
-
-    create_boolean_queries_file(boolean_query_prompt, filename=boolean_query_file)
-
-    # @todo Prendre en charges d'autres bases de données (implique de modifier le prompt aussi)
-
-    excel_file = "articles.xlsx"
-    sheet_name1 = "Articles"
-    sheet_name2 = "Conjectures"
-    create_excel_file(excel_file, sheet_name1, sheet_name2)
-
-    query = extract_arxiv_query_py(boolean_query_file)
-    download_arxiv_pdfs(query, excel_file=excel_file, sheet_name=sheet_name1)
+    # research_question = get_research_question_arg()
+    # boolean_query_file = "boolean_queries.txt"
+    # pico_prompt = get_prompt_to_generate_pico(research_question, f"PICO{research_question}.txt")
+    #
+    # create_pico_file(pico_prompt)
+    #
+    # pico_filename = f"PICO_{research_question}.txt"
+    #
+    # boolean_query_prompt = get_prompt_to_generate_boolean_query(
+    #     pico_filename=pico_filename,
+    #     research_question=research_question,
+    #     output_file_name_boolean_queries="boolean_queries",
+    #     doc_boolean_queries="doc_boolean_queries",
+    # )
+    #
+    # create_boolean_queries_file(boolean_query_prompt, filename=boolean_query_file)
+    #
+    # # @todo Prendre en charges d'autres bases de données (implique de modifier le prompt aussi)
+    #
+    # excel_file = "articles.xlsx"
+    # sheet_name1 = "Articles"
+    # sheet_name2 = "Conjectures"
+    # create_excel_file(excel_file, sheet_name1, sheet_name2)
+    #
+    # query = extract_arxiv_query_py(boolean_query_file)
+    # download_arxiv_pdfs(query, excel_file=excel_file, sheet_name=sheet_name1)
 
     print("Process completed.")
 
     # todo : voir pour extraire le contenu des articles au lieu de les uploads pour contourner la limite d'upload.
-    find_conjectures("downloads/arxiv")
+    # find_conjectures("downloads/arxiv")
+    find_conjectures("test")
 
     update_excel_with_conjectures("articles.xlsx", "Articles", "Conjectures", Path("json_articles"))
 
